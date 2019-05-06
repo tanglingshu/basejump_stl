1bc223804c374cc2fa23a045236c627bbb14d0b3
Lingshu Tang <lingshu@linux13.ece.uw.edu>
2019-05-06 12:50:34 -0700

    a synthesized multiplier used for default multiplier testing

---
+++ bsg_misc/bsg_mult_synth.v
@@ -0,0 +1,9 @@
+module bsg_mult_synth #(parameter width_p=8)
+	(input signed [width_p-1:0] in_a
+	,input signed [width_p-1:0] in_b
+	,output signed [2*width_p-1:0] out
+	);
+
+	out = in_a * in_b;
+
+endmodule
